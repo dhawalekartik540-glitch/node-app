3d89210c89dee126108e3018f15ca8bd1fd2535a
dhawalekartik540-glitch <dhawalekartik540@gmail.com>
2026-06-05 16:39:40 +0530

    cred added

--- node.groovy
+++ node.groovy
@@ -81,7 +81,7 @@ pipeline {
             steps {
                 withCredentials([
                     usernamePassword(
-                        credentialsId: 'dockerhub-creds',
+                        credentialsId: 'ac30b12e-a7de-4654-ae7f-fcf480c943e6',
                         usernameVariable: 'DOCKER_USERNAME',
                         passwordVariable: 'DOCKER_PASSWORD'
                     )
