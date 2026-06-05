c4e2584c95d3dc20620d277e9d5058f7ea1f1271
dhawalekartik540-glitch <dhawalekartik540@gmail.com>
2026-06-05 17:07:13 +0530

    k

--- node.groovy
+++ node.groovy
@@ -54,7 +54,7 @@ pipeline {
             steps {
                 withCredentials([
                     usernamePassword(
-                        credentialsId: '626460db-bf4f-41a8-86b7-e03f88673be7',
+                        credentialsId: 'dockerhub-credentials',
                         usernameVariable: 'DOCKER_USERNAME',
                         passwordVariable: 'DOCKER_PASSWORD'
                     )
